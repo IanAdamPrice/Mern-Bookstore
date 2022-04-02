2079e2529d7b5f0472a8f6eb2206862c94a77cf2
IanAdamPrice <ianadamprice@gmail.com>
2022-04-02 11:04:34 -0400

    started resolvers

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,2 +1,38 @@
 const { AuthenticationError } = require('apollo-server-express');
-const { User, Book } = require('../models');
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+const resolvers = {
+  Query: {
+    me: async (parent, args, context) => {
+      if (context.user) {
+        const userData = await User.findOne({ _id: context.user._id })
+          .select('-__v -password')
+          .populate('savedBooks');
+        
+          return userData
+      }
+
+      throw new AuthenticationError('Not logged in');
+    },
+    users: async () => {
+      return User.find()
+        .select('-__v -password')
+        .populate('savedbooks')
+    },
+    user: async (parent, { username }) => {
+      return User.findOne({ username })
+        .select('-__v -password')
+        .populate('savedbooks')
+    }
+  },
+
+  Mutation: {
+    addUser: async (parent, args) => {
+      const user = await User.create(args);
+      const token = signToken(user);
+
+      return { token, user };
+    },
+  }
+}
--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const {ApolloServer} = require('apollo-server-express');
 const path = require('path');
+const routes = require('./routes');
+
 
 const {typeDefs, resolvers} = require('./schemas');
 const {authMiddleware} = require('./utils/auth')
@@ -9,19 +11,14 @@ const db = require('./config/connection');
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const startServer = async () => {
-  const server = new ApolloServer({
+const server = new ApolloServer({
     typeDefs,
     resolvers,
     context: authMiddleware,
-  });
-  await server.start();
-  server.applyMiddleware({ app })
-  console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
-};
+});
 
-startServer()
-
+server.applyMiddleware({ app })
+console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -30,6 +27,8 @@ app.use(express.json());
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
+
+app.use(routes);
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../client/build/index.html'));
